9f8667f0b27d120be2cba54349c57ce1983cd1df
patrick-motard <motard19@gmail.com>
2017-07-29 01:50:22 -0500

    fixed setup bugs with sudo, added yaourt to setup

--- .local/bin/han_setup.zsh
+++ .local/bin/han_setup.zsh
@@ -1,24 +1,24 @@
 ## update pacman
-pacman -Syu
+sudo pacman -Syu
 
 for package_name (
   keychain  ## used for maintaining ssh-agent keys
   python-pip ## pip used for python packages
   emacs  
 )
-; pacman -S $package_name --needed --noconfirm
+; sudo pacman -S $package_name --needed --noconfirm
 
-##  keychain,\
- ##   python-pip,\ 
- ##   emacs \
- ##   --noconfirm ## install without asking for confirmation
- ##   --needed ## dont re-install, install only
+for yaourt_package (
+  ttf-font-awesome
+)
+; yaourt $yaourt_package --noconfirm
 
+## Python package installs
 ## Install wakatime
 pip install wakatime
 
 ## Install Vundle
-if [[ ! -d /home/han/.vim/bundle/ ]]; then
+if [[ ! -d /home/han/.vim/bundle/Vundle.vim ]]; then
   git clone https://github.com/VundleVim/Vundle.vim.git /home/han/.vim/bundle/Vundle.vim
 else
   cd /home/han/.vim/bundle/Vundle.vim && git pull && echo 'hello world'
