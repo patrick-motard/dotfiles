24719425ec7cd9a46490e067cc0f72ad5c7c26c1
patrick-motard <motard19@gmail.com>
2017-07-30 18:59:32 -0500

    nvm installing + tern + lts/boron for real this time

--- .local/bin/han_setup.bash
+++ .local/bin/han_setup.bash
@@ -1,7 +1,7 @@
 ## update pacman
 sudo pacman -Syu --noconfirm
 ## rank mirrors by speed and filter out out of date mirrors
-sudo pacman-mirrors -g
+##sudo pacman-mirrors -g
 sudo pacman-optimize && sync
 
 export pacman_packages="
@@ -10,7 +10,7 @@ export pacman_packages="
   virtualbox-guest-utils
   virtualbox-guest-modules-arch
   vim
-  zsh 
+  zsh
   keychain
   python-pip
   firefox
@@ -19,6 +19,7 @@ export pacman_packages="
   dropbox
   emacs
   nitrogen
+  compton
 "
 
 for package_name in $pacman_packages; do
@@ -27,8 +28,8 @@ done
 
 ## set zsh to default shell
 if [[ "$(echo $SHELL)" != "/usr/bin/zsh" ]]; then
-	chsh -s $(which zsh)
-	echo "shell set to zsh"
+  chsh -s $(which zsh)
+  echo "shell set to zsh"
 fi
 
 ## Install oh-my-zsh
@@ -44,9 +45,13 @@ if [[ ! -d /home/han/.vim/bundle/Vundle.vim ]]; then
 fi
 
 ## Install NVM if nvm command not recognized
-if command -v nvm >/dev/null 2>&1; then  
+if ! type nvm > /dev/null; then
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+  . ~/.nvm/nvm.sh ## source nvm
+  nvm install lts/boron
+  npm i -g tern ## tern is used for the javascript layer in emacs
 fi
+
 ## Set yadm remote to .ssh
 yadm remote set-url origin git@github.com:patrick-motard/dotfiles.git
 
@@ -57,8 +62,8 @@ fi
 
 ## use vmware setupscript (works with virtualbox)
 if [[ ! -d ~/downloads/vmware-tools/.git ]]; then
-git clone https://github.com/rasa/vmware-tools-patches.git ~/downloads/vmware-tools/
-sudo sh /home/han/downloads/vmware-tools/patched-open-vm-tools.sh
+  git clone https://github.com/rasa/vmware-tools-patches.git ~/downloads/vmware-tools/
+  sudo sh /home/han/downloads/vmware-tools/patched-open-vm-tools.sh
 fi
 
 ## install yaourt packages
@@ -67,6 +72,7 @@ export yaourt_packages="
   i3-gaps-git
   i3blocks-gaps-git
 "
+
 for yaourt_package in $yaourt_packages; do
   yaourt $yaourt_package --noconfirm
 done
