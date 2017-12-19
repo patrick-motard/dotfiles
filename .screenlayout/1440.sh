f8de2c200f5adb2dd7ac86e48bb31fc86a3f24ea
patrick-motard <motard19@gmail.com>
2017-12-19 14:14:37 -0600

    arandr mode, dual/single monitor mode switch
    closes #42

---
+++ .screenlayout/1440.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DVI-I-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DVI-I-0 --off --output DP-1 --off --output DP-0 --off
