b23847760ecad1f6ad85a36d6f923554d21ba513
patrick-motard <motard19@gmail.com>
2017-12-19 14:14:37 -0600

    arandr mode, dual/single monitor mode switch
    closes #42

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,7 +4,7 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -r bottom &
-polybar -r example &
+polybar -r main &
+polybar -r left &
 
 echo "Bars launched..."
