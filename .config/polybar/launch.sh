1a592d3bf170c3865c0c63d86deb62f75d82f9e3
patrick-motard <motard19@gmail.com>
2018-01-15 13:05:40 -0600

    environment variables passed into polybar when called from i3 now working

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+active_monitors=$(xrandr -q | grep "connected" | awk "{print $"${1:-1}"}")
+echo $active_monitors
+# source set-monitors
 
 killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -r main &
+MONITOR_MAIN="$active_monitors" polybar -r main &
 polybar -r left &
 
 echo "Bars launched..."
