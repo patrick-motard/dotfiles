3fbba7e145fb987816dab1de24a93cba11e9e432
patrick-motard <motard19@gmail.com>
2018-01-31 12:07:32 -0600

    reduced duplication in polybar launch script. updated spacemacs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -16,31 +16,39 @@ active_monitors=$(xrandr -q | grep " connected" | awk "{print $"${1:-1}"}" ORS="
 
 # # my monitor configurations
 # # replace with your own (based on the output above)
-work="DVI-I-1 DVI-D-0 "
+work_desktop="DVI-I-1 DVI-D-0 "
 work_laptop="VGA-1 "
+home_desktop="HDMI-0 DP-0 "
 
+function export_monitor_vars() {
+    echo $1 $2 $3
+    export MONITOR_MAIN=$1
+    export MONITOR_RIGHT=$2
+    export MONITOR_LEFT=$3
+}
 
-case "${active_monitors}" in
-    $work )
-        MONITOR_MAIN="DVI-I-1"
-        MONITOR_RIGHT="DVI-D-0"
-        MONITOR_LEFT=""
-        mode="work"
-        ;;
-    $work_laptop )
-        MONITOR_MAIN="VGA-1"
-        MONITOR_RIGHT=""
-        MONITOR_LEFT=""
-        mode="work laptop"
-        ;;
-    * )
-        notify-send "Polybar" "Monitor configuration not recognized. See ~/.config/polybar/launch.sh for details"
-        ;;
-esac
+function set_monitor_vars() {
+    case "${active_monitors}" in
+        $work_desktop )
+            export_monitor_vars "DVI-I-1" "DVI-D-0" ""
+            mode="work"
+            ;;
+        $work_laptop )
+            export_monitor_vars "VGA-1" "" ""
+            mode="work laptop"
+            ;;
+        $home_desktop )
+            export_monitor_vars "DP-0" "" "HDMI-0"
+            mode="home desktop"
+            ;;
+        * )
+            notify-send "Polybar" "Monitor configuration not recognized. See ~/.config/polybar/launch.sh for details"
+            ;;
+    esac
+}
 
-export MONITOR_MAIN=$MONITOR_MAIN
-export MONITOR_RIGHT=$MONITOR_RIGHT
-export MONITOR_LEFT=$MONITOR_LEFT
+set_monitor_vars
+notify-send "Polybar" "Bars initialized on ${mode} monitors."
 
 killall -q polybar
 
@@ -49,6 +57,5 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar -r main &
 polybar -r right &
 
-notify-send "Polybar" "Bars initialized on ${mode} monitors."
 
 echo "Bars launched..."
