6fc2bbbd0c77d60d556557f49c14f7ed0bd8aa49
patrick-motard <motard19@gmail.com>
2018-11-29 20:01:36 -0600

    polybar: home desktop now uses 3 monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -19,7 +19,8 @@ active_monitors=$(xrandr -q | grep " connected" | awk "{print $"${1:-1}"}" ORS="
 work_desktop="DVI-I-1 DVI-D-0 "
 work_desktop_4k="DVI-I-1 "
 work_laptop="VGA-1 "
-home_desktop="HDMI-0 DP-0 "
+#home_desktop="HDMI-0 DP-4 "
+home_desktop="DVI-D-0 HDMI-0 DP-4 "
 
 function export_monitor_vars() {
     export MONITOR_MAIN=$1
@@ -42,7 +43,7 @@ function set_monitor_vars() {
             mode="work laptop"
             ;;
         $home_desktop )
-            export_monitor_vars "DP-0" "" "HDMI-0"
+            export_monitor_vars "DP-4" "DVI-D-0" "HDMI-0"
             mode="home desktop"
             ;;
         * )
