27d962364946b8ecd7da9815a1d51926f54bf0df
patrick-motard <motard19@gmail.com>
2018-12-27 17:38:50 -0600

    yadm|polybar: big facelift
    - module now has better icons
    - changes background colors based on status
    - now also tracks commits ahead/behind origin/master

--- .local/bin/tools/yadm-checker.sh
+++ .local/bin/tools/yadm-checker.sh
@@ -1,12 +1,90 @@
 #!/usr/bin/env bash
+
+###################
+## COLOR PALETTE ##
+###################
+
+# [colors]
+# Nord
+# ===================== #
+# blacks
+nord0="#2E3440"
+nord1="#3B4252"
+nord2="#434C5E"
+nord3="#4C566A"
+# whites
+nord4="#D8DEE9"
+nord5="#E5E9F0"
+nord6="#ECEFF4"
+
+# ===================== #
+# blues 
+# seafoam
+nord7="#8FBCBB"
+# turquoise
+nord8="#88C0D0"
+# pastel
+nord9="#81A1C1"
+# blue
+nord10="#5E81AC"
+
+# ===================== #
+# red
+nord11="#BF616A"
+# orange
+nord12="#D08770"
+# yellow
+nord13="#EBCB8B"
+# green
+nord14="#A3BE8C"
+# purple
+nord15="#B48EAD"
+
+yadm fetch > /dev/null 2>&1
+
 staged=$(yadm diff --cached --numstat | wc -l)
 modified=$(yadm ls-files -m | wc -l)
+# shows how many commits ahead and behind local master is from origin
+ahead_behind_count=$(yadm rev-list --left-right --count master...origin/master)
+ahead=$(echo $ahead_behind_count | cut -d ' ' -f1)
+behind=$(echo $ahead_behind_count | cut -d ' ' -f2)
 
-no= 
+ahead_icon=
+behind_icon=
+yadm_icon=
+
+
+no=
 yes=
+modified_icon=
+staged_icon=
+commits_prompt="  $yadm_icon $behind $behind_icon $ahead $ahead_icon "
+
+
+function color {
+    echo "%{B${1}}%{F${2}}${3}%{B-}%{F-}"
+}
+
+if [[ "$behind" -gt 0 ]]; then
+    fg=$nord11
+    commits_prompt=$(color "${nord11}" "${nord1}" "${commits_prompt}")
+elif [[ "$ahead" -gt 0 ]]; then
+    fg=$nord13
+    commits_prompt=$(color "${nord13}" "${nord1}" "${commits_prompt}")
+else
+    fg=$nord14
+    commits_prompt=$(color "${nord14}" "${nord1}" "${commits_prompt}")
+fi
+
 
 if [[ $staged != "0" || $modified != "0" ]]; then
-    echo " %{F#BF616A}$no%{F-} s:$staged | m:$modified"
+
+    bg=$nord13
+    staged_prompt=$(color "${nord13}" "${nord1}" " $staged_icon $staged $modified_icon $modified  $no ")$(color "${nord0}" "${nord13}" " ")
 else
-    echo " %{F#A3BE8C}$yes%{F-} s:$staged | m:$modified"
+    bg=$nord14
+    staged_prompt=$(color "${nord14}" "${nord1}" " $staged_icon $staged $modified_icon $modified  $yes ")$(color "${nord0}" "${nord14}" " ")
 fi
+
+
+echo $commits_prompt$(color "${bg}" "${fg}" " ")$staged_prompt
