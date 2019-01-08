b399efe76a051c9bf891ceba2148af1a77b05694
patrick-motard <motard19@gmail.com>
2019-01-08 12:10:43 -0600

    polybar: launch script updated and improved
    - export_monitor_vars now takes parameters for monitors in order left,
    main, then right. Used to be main, right, left, which always confused
    me.
    - work now uses 3 monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -16,34 +16,28 @@ active_monitors=$(xrandr -q | grep " connected" | awk "{print $"${1:-1}"}" ORS="
 
 # my monitor configurations
 # replace with your own (based on the output above)
-work_desktop="DVI-I-1 DVI-D-0 "
-work_desktop_4k="DVI-I-1 "
+work_desktop="DVI-I-1 DP-1 DVI-D-0 "
 work_laptop="VGA-1 "
-#home_desktop="HDMI-0 DP-4 "
 home_desktop="DVI-D-0 HDMI-0 DP-4 "
 
 function export_monitor_vars() {
-    export MONITOR_MAIN=$1
-    export MONITOR_RIGHT=$2
-    export MONITOR_LEFT=$3
+    export MONITOR_LEFT=$1
+    export MONITOR_MAIN=$2
+    export MONITOR_RIGHT=$3
 }
 
 function set_monitor_vars() {
     case "${active_monitors}" in
         $work_desktop )
-            export_monitor_vars "DVI-I-1" ""  "" #"DVI-D-0"
-            mode="work"
-            ;;
-        $work_desktop_4k )
-            export_monitor_vars "DVI-I-1" "" ""
+            export_monitor_vars "DVI-I-1" "DVI-D-0" "DP-1" #"DVI-I-1" ""  "" #"DVI-D-0"
             mode="work"
             ;;
         $work_laptop )
-            export_monitor_vars "VGA-1" "" ""
+            export_monitor_vars "" "VGA-1" ""
             mode="work laptop"
             ;;
         $home_desktop )
-            export_monitor_vars "DP-4" "DVI-D-0" "HDMI-0"
+            export_monitor_vars "HDMI-0" "DP-4" "DVI-D-0"
             mode="home desktop"
             ;;
         * )
