15c204142ee921e13f2e45e9b09320c1a9bec542
patrick-motard <motard19@gmail.com>
2019-08-12 01:53:51 -0500

    added non spacemacs emacs folder

---
+++ .emacs.d.han/elpa/cycle-themes-20150403.309/cycle-themes-autoloads.el
@@ -0,0 +1,37 @@
+;;; cycle-themes-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "cycle-themes" "cycle-themes.el" (0 0 0 0))
+;;; Generated autoloads from cycle-themes.el
+
+(defvar cycle-themes-mode nil "\
+Non-nil if Cycle-Themes mode is enabled.
+See the `cycle-themes-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `cycle-themes-mode'.")
+
+(custom-autoload 'cycle-themes-mode "cycle-themes" nil)
+
+(autoload 'cycle-themes-mode "cycle-themes" "\
+Minor mode for cycling between themes.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "cycle-themes" '("cycle-themes")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; cycle-themes-autoloads.el ends here
