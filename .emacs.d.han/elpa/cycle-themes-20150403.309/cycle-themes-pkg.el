af86affef9ad6d2d5e7ae21768a10c59e5f2c392
patrick-motard <motard19@gmail.com>
2019-08-12 01:53:51 -0500

    added non spacemacs emacs folder

---
+++ .emacs.d.han/elpa/cycle-themes-20150403.309/cycle-themes-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cycle-themes" "20150403.309" "A global minor mode to make switching themes easier" '((cl-lib "0.5")) :commit "6e125d11fdbc6b78fc9f219eb2609a5e29815898" :keywords '("themes" "utility" "global minor mode") :url "http://github.com/toroidal-code/cycle-themes.el")
