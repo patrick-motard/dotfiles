46801569883114f2303d8052d60ae729a389e827
patrick-motard <motard19@gmail.com>
2019-08-22 22:59:05 -0500

    removed han emacs files

--- .emacs.d.han/elpa/go-mode-20190814.157/go-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "go-mode" "20190814.157" "Major mode for the Go programming language" 'nil :commit "d387378ca3c1241d7b2fa8e63804e273851830e4" :keywords '("languages" "go") :authors '(("The go-mode Authors")) :maintainer '("The go-mode Authors") :url "https://github.com/dominikh/go-mode.el")
