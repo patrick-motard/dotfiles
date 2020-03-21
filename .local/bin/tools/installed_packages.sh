fdce5e2c0455a2496bd92419eee9efbde4b58174
patrick-motard <motard19@gmail.com>
2020-03-21 11:50:00 -0500

    removed scripts, moving them to their own repo.
    don't want code in this repo, only configuration.
    dot-ansible will clone the scripts repo, then symlink it to the
    directory this same tools directory

--- .local/bin/tools/installed_packages.sh
+++
@@ -1,5 +0,0 @@
-# output list of installed packages via pacman to the a file
-pacman -Qqe | grep -vx "$(pacman -Qqm)" > ~/.config/pacman-packages
-
-# same for packages installed via yaourt
-pacman -Qqm > ~/.config/yaourt-packages
