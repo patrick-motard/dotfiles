15d2d31870778bbdbb485e3dae75bfc22d36d777
Patrick Motard <motard19@gmail.com>
2024-02-15 23:16:03 -0600

    added alias to update vim files. switched vim theme to gruvbox.

---
+++ dot_config/nvim/lua/plugins/main.lua
@@ -0,0 +1,11 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
