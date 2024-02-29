42f6bb0287dda3e065e538502adeb8bb20039168
Patrick Motard <motard19@gmail.com>
2024-02-29 09:35:09 -0600

    fixed fd map

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,5 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-vim.keymap.set("iv", "fd", "<ESC>", { silent = true })
+vim.keymap.set("i", "fd", "<ESC>", { silent = true })
 vim.keymap.set("n", "<leader>bt", "<Cmd>BufferLineTogglePin<CR>")
