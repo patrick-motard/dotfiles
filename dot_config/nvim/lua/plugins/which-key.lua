776a5ca9da01401003c3f173a0ada67d32eda173
Patrick Motard <motard19@gmail.com>
2024-02-28 21:00:16 -0600

    nvim vim keybinds for moving windows

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -2,5 +2,9 @@ return {
   "folke/which-key.nvim",
   keys = {
     { "<leader>cf", ':let @+ = expand("%?")<cr>', desc = "Copy file path." },
+    { "<leader>wh", "<C-w>h", desc = "Go ←" },
+    { "<leader>wl", "<C-w>l", desc = "Go →" },
+    { "<leader>wj", "<C-w>j", desc = "Go ↓" },
+    { "<leader>wk", "<C-w>k", desc = "Go ↑" },
   },
 }
