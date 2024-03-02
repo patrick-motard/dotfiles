3c20883268c99dd2e3f031be84ede11122de2974
Patrick Motard <motard19@gmail.com>
2024-03-02 15:29:03 -0600

    added somekeybinds that don't work

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -6,5 +6,8 @@ return {
     { "<leader>wl", "<C-w>l", desc = "Go →" },
     { "<leader>wj", "<C-w>j", desc = "Go ↓" },
     { "<leader>wk", "<C-w>k", desc = "Go ↑" },
+    -- { "<leader>cs", "<cmd>term ma<cr>", desc = "Source dotfiles" },
+    -- { "<leader>w/", "<Cmd>vsplit<CR>", desc = "Split Vertical" },
+    -- { "<leader>w-", "<Cmd>split<CR>", desc = "Split Horizontal" },
   },
 }
