f0ba7c5fe0bf33efd0a00a3022d64e8481cb9d50
Patrick Motard <motard19@gmail.com>
2024-03-10 23:04:21 -0500

    added several color schemes

--- dot_config/nvim/lua/plugins/main.lua
+++ dot_config/nvim/lua/plugins/main.lua
@@ -1,11 +1,16 @@
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
+  { "shaunsingh/nord.nvim" },
+  { "sainnhe/gruvbox-material" },
+  { "sainnhe/edge" },
+  { "AlexvZyl/nordic.nvim" },
+  { "savq/melange-nvim" },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "nord",
     },
   },
 }
