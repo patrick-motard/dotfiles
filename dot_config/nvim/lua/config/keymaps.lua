820d90dd87c14c150e5dc6d7ae6443a8b118035e
Patrick Motard <motard19@gmail.com>
2024-03-15 14:31:10 -0500

    open ruby test file hotkey wip

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,5 +2,15 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-vim.keymap.set("i", "fd", "<ESC>", { silent = true })
+-- vim.keymap.set("i", "fd", "<ESC>", { silent = true })
 vim.keymap.set("n", "<leader>bt", "<Cmd>BufferLineTogglePin<CR>")
+
+local home = os.getenv("HOME")
+
+local function get_test_file(filename)
+  vim.notify(filename)
+end
+
+vim.keymap.set("n", "<leader>o", function()
+  get_test_file(vim.fn.expand("%:t"))
+end, { silent = true, noremap = true })
