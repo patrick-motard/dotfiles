52357fb08f6d269eed9dec99b19363dee7c83578
Patrick Motard <motard19@gmail.com>
2024-06-04 23:48:26 -0500

    neovim now automatically cleans trailing whitespace

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -7,6 +7,16 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     vim.schedule(require("chezmoi.commands.__edit").watch)
   end,
 })
+local function remove_trailing_whitespace()
+  vim.cmd([[ %s/\s/+$//e ]])
+end
+
+vim.api.nvim_create_augroup("TrimWhitespaceGroup", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = "TrimWhitespaceGroup",
+  pattern = "*",
+  callback = remove_trailing_whitespace,
+})
 
 -- vim.api.nvim_create_augroup("dotfile_cmds", { clear: true })
 -- vim.api.nvim_create_autocmd("BufWritePost", { "BufWritePost",
