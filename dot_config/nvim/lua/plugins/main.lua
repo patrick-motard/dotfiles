bd8edfe20fa22c3d6c71bf2b5ce8c1bd29567e73
Patrick Motard <motard19@gmail.com>
2024-06-13 21:39:38 -0500

    neovim added tokyonight theme

--- dot_config/nvim/lua/plugins/main.lua
+++ dot_config/nvim/lua/plugins/main.lua
@@ -4,11 +4,12 @@ return {
   { "shaunsingh/nord.nvim" },
   { "sainnhe/gruvbox-material" },
   { "sainnhe/everforest" },
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      colorscheme = "tokyonight",
     },
   },
   { "google/vim-jsonnet" },
