192865e1e678b942b13301013e27d672ebfb7ec9
Patrick Motard <motard19@gmail.com>
2024-07-30 22:07:02 -0500

    nvim: added oil.nvim plugin

---
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,12 @@
+return {
+  "stevearc/oil.nvim",
+  opts = {
+    default_file_explores = true,
+    win_options = {
+      wrap = true,
+    },
+  },
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+}
