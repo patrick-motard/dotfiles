5577228540af130f6474ac624a82344a1ff28b6e
Patrick Motard <motard19@gmail.com>
2024-08-02 16:23:58 -0500

    nvim: changed colorscheme

--- dot_config/nvim/lua/plugins/main.lua
+++ dot_config/nvim/lua/plugins/main.lua
@@ -2,15 +2,17 @@ return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
   { "shaunsingh/nord.nvim" },
+  { "AlexvZyl/nordic.nvim" },
   { "sainnhe/gruvbox-material" },
   { "sainnhe/everforest" },
   { "ptdewey/darkearth-nvim", priority = 1000 },
   { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
   -- Configure LazyVim to load gruvbox
+  -- This is how you set the default theme.
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "gruvbox-material",
     },
   },
   { "google/vim-jsonnet" },
