32f6bb860783d5ed593ec4ad61ab463ac62c30fa
Patrick Motard <motard19@gmail.com>
2024-08-04 21:30:55 -0500

    nvim: editing cheatsheet now takes effect on save

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,32 +1,27 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
---
--- vim.keymap.set("i", "fd", "<ESC>", { silent = true })
-vim.keymap.set("n", "<leader>bt", "<Cmd>BufferLineTogglePin<CR>")
+-- Notes:
+-- To send a notification in vim:
+-- vim.notify(filename)
+-- This is how to do a nested group in whichkey, for reference.
+-- vim.keymap.set("n", "<leader>hh", "+hotkeys", { silent = true, noremap = true })
 
-local home = os.getenv("HOME")
+-- Keymaps:
 
+-- Ruby Related
+------------------------------------------------------------------------------------------------------------
+-- Find and open test file for currently open ruby file.
 local function get_test_file(filename)
-  -- vim.notify(filename)
-  -- spec/unit/app/controllers/api/base_controller_spec.rb
-  --domains/product_catalog/api.rb
   local test_path = string.format("spec/unit/%s_spec.rb", filename)
-  -- vim.notify(test_path)
   vim.cmd.edit(test_path)
 end
-
 vim.keymap.set("n", "<leader>ot", function()
   get_test_file(vim.fn.expand("%:r"))
-  -- get_test_file(vim.fn.expand("%:t"))
 end, { silent = true, noremap = true, desc = "Open test for ruby file." })
 
---
--- chezmoi
--- vim.keymap.set()
+-- Cheatsheet Plugin Related
+------------------------------------------------------------------------------------------------------------
+-- Open Cheatsheet
 vim.keymap.set("n", "<leader>hhl", "<Cmd>Cheatsheet<CR>", { desc = "list", silent = true, noremap = true })
-vim.keymap.set("n", "<leader>hh", "+hotkeys", { silent = true, noremap = true })
-
-local k = vim.keymap
-
-k.set("n", "<leader>hhe", "<Cmd>CheatsheetEdit<CR>", { desc = "edit", silent = true, noremap = true })
+-- Edit Cheatsheet
+vim.keymap.set("n", "<leader>h", function()
+  vim.cmd.edit(os.getenv("MOIDIR") .. "/dot_config/nvim/cheatsheet.txt")
+end, { silent = true, noremap = true, desc = "Edit Cheatsheet" })
