2d8a661a9e5f904960c149b8dd5e8744cfa0c731
Patrick Motard <motard19@gmail.com>
2024-08-06 00:00:40 -0500

    nvim: attempting to make cursor blink faster

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -16,6 +16,7 @@ local options = {
   -- https://neovim.io/doc/user/options.html#'timeoutlen'
   -- How quickly vim will respond to a mapped key
   timeoutlen = 50,
+  guicursor = "n-v-c-sm:block-blinkon100,i-ci-ve:ver25-blinkon100,r-cr-o:hor20-blinkon100",
 }
 
 for k, v in pairs(options) do
