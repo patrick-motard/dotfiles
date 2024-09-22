2bb8e572d2d5d6f10ade3b4b5926e22b78b8d249
Patrick Motard <motard19@gmail.com>
2024-09-21 21:44:57 -0500

    nvim: custom: added indent line plugin
    shows indentation lines

---
+++ dot_config/nvim-custom/lua/custom/plugins/indent_line.lua
@@ -0,0 +1,11 @@
+return {
+	{ -- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help ibl`
+		---@module "ibl"
+		---@type ibl.config
+		main = "ibl",
+		opts = {},
+	},
+}
