14941e93ab3fd89baaa91c146f51ee89e3d01a82
Patrick Motard <motard19@gmail.com>
2024-09-24 15:35:13 -0500

    nvim: custom: added git-blame

---
+++ dot_config/nvim-custom/lua/custom/plugins/git_blame.lua
@@ -0,0 +1,11 @@
+-- plugins/git-blame.lua
+return {
+  {
+    'f-person/git-blame.nvim',
+    -- config = function()
+    --   require("git-blame").setup({
+    --     message_when_not_committed = " ",
+    --   })
+    -- end,
+  },
+}
