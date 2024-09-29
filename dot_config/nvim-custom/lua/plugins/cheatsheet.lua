eed41af19c8c29130dcb652293d11af42c619909
Patrick Motard <motard19@gmail.com>
2024-09-29 15:00:56 -0500

    nvim: added cheatsheet hotkeys

--- dot_config/nvim-custom/lua/plugins/cheatsheet.lua
+++ dot_config/nvim-custom/lua/plugins/cheatsheet.lua
@@ -1,14 +1,23 @@
 return {
-  "sudormrfbin/cheatsheet.nvim",
+  'sudormrfbin/cheatsheet.nvim',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
-    "nvim-lua/popup.nvim",
-    "nvim-lua/plenary.nvim",
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+    'nvim-lua/popup.nvim',
+    'nvim-lua/plenary.nvim',
   },
   config = function()
-    require("cheatsheet").setup({
+    require('cheatsheet').setup {
       bundled_cheatsheets = false,
-    })
+    }
+
+    -- Cheatsheet Plugin Related
+    ------------------------------------------------------------------------------------------------------------
+    -- Open Cheatsheet
+    vim.keymap.set('n', '<leader>hh', '<Cmd>Cheatsheet<CR>', { desc = 'Search Cheatsheet', silent = true, noremap = true })
+    -- Edit Cheatsheet
+    vim.keymap.set('n', '<leader>he', function()
+      vim.cmd.edit(os.getenv 'MOIDIR' .. '/dot_config/nvim/cheatsheet.txt')
+    end, { silent = true, noremap = true, desc = 'Edit Cheatsheet' })
   end,
 }
