9033481d4edd8512a5f062c8e16bf1e40aba2b34
Patrick Motard <motard19@gmail.com>
2024-09-28 21:42:58 -0500

    nvim: ported autocommands

--- dot_config/nvim-custom/lua/custom/autocmds.lua
+++ dot_config/nvim-custom/lua/custom/autocmds.lua
@@ -4,3 +4,13 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
     vim.schedule(require('chezmoi.commands.__edit').watch)
   end,
 })
+local function remove_trailing_whitespace()
+  vim.cmd [[ %s/\s\+$//e ]]
+end
+
+vim.api.nvim_create_augroup('TrimWhitespaceGroup', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = 'TrimWhitespaceGroup',
+  pattern = '*',
+  callback = remove_trailing_whitespace,
+})
