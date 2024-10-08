2559d20694b6320d88b5cf88ad28ce142ed035f1
Patrick Motard <motard19@gmail.com>
2024-10-08 11:24:29 -0500

    nvim: window movement keymaps rebound to colemak

--- dot_config/nvim-custom/lua/config/keymaps.lua
+++ dot_config/nvim-custom/lua/config/keymaps.lua
@@ -13,21 +13,15 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
+vim.api.nvim_set_keymap('n', '<C-w>m', '<C-w>h', { noremap = true, silent = true }) -- Move left (C-w h)
+vim.api.nvim_set_keymap('n', '<C-w>n', '<C-w>j', { noremap = true, silent = true }) -- Move down (C-w j)
+vim.api.nvim_set_keymap('n', '<C-w>e', '<C-w>k', { noremap = true, silent = true }) -- Move up (C-w k)
+vim.api.nvim_set_keymap('n', '<C-w>i', '<C-w>l', { noremap = true, silent = true }) -- Move right (C-w l)
 -- { '<leader>wh', '<C-w>h', desc = 'Go ←' },
 -- { '<leader>wl', '<C-w>l', desc = 'Go →' },
 -- { '<leader>wj', '<C-w>j', desc = 'Go ↓' },
