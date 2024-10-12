30db481ec6e67ed097b7446e6f700e7a6a9f4669
Patrick Motard <motard19@gmail.com>
2024-10-12 13:05:12 -0500

    nvim: disable column cursor highlight. I don't like it.

--- dot_config/nvim-custom/lua/config/options.lua
+++ dot_config/nvim-custom/lua/config/options.lua
@@ -67,7 +67,7 @@ vim.opt.inccommand = 'split'
 
 -- Show which line and column your cursor is on
 vim.opt.cursorline = true
-vim.opt.cursorcolumn = true
+-- vim.opt.cursorcolumn = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
