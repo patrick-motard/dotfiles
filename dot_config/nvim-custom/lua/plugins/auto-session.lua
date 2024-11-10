316d358709ac17121b8d79c6a08da58d8220ec4d
Patrick Motard <motard19@gmail.com>
2024-11-10 14:16:27 -0600

    nvim: added auto session

---
+++ dot_config/nvim-custom/lua/plugins/auto-session.lua
@@ -0,0 +1,14 @@
+-- https://github.com/rmagatti/auto-session
+return {
+  'rmagatti/auto-session',
+  lazy = false,
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    -- suppressed_dirs = { '~/' },
+    allowed_dirs = { '~/code/**', '~/.local/share/chezmoi/**', '~/.config/', '~/.ssh/' },
+    -- log_level = 'debug',
+  },
+}
