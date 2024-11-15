c529ac8fa5613d74c367f6736ed564a5ff7506b1
Patrick Motard <motard19@gmail.com>
2024-11-14 22:38:19 -0600

    nvim: added additional telescope hotkeys

--- dot_config/nvim-custom/lua/plugins/telescope.lua
+++ dot_config/nvim-custom/lua/plugins/telescope.lua
@@ -80,15 +80,23 @@ return {
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = '[S]earch [F]iles' })
-    -- Gives a list o telescope searches that can be made.
+    vim.keymap.set('n', '<leader><leader>', function()
+      builtin.find_files {
+        -- show hidden files
+        hidden = true,
+        -- show gitignored files
+        no_ignore = true,
+      }
+    end, { desc = '[S]earch [F]iles' })
+    -- Gives a list o telescope searches that can be mad.
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader>,', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>,', builtin.buffers, { desc = '[,] Find existing buffers' })
+    vim.keymap.set('n', '<leader>sc', builtin.command_history, { desc = '[s]earch [c]ommand history' })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
