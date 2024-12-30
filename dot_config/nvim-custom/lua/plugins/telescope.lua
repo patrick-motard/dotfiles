9137432936204a78651a91f9ca37b7eba945e170
Patrick Motard <motard19@gmail.com>
2024-12-30 15:09:33 -0600

    nvim telescope find existing buffer now sorts by most recently used.

--- dot_config/nvim-custom/lua/plugins/telescope.lua
+++ dot_config/nvim-custom/lua/plugins/telescope.lua
@@ -95,7 +95,10 @@ return {
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader>,', builtin.buffers, { desc = '[,] Find existing buffers' })
+    vim.keymap.set('n', '<leader>,', function()
+      builtin.buffers { sort_mru = true, ignore_current_buffer = true }
+    end, { desc = '[,] Find existing buffers' })
+
     vim.keymap.set('n', '<leader>sc', builtin.command_history, { desc = '[s]earch [c]ommand history' })
 
     -- Slightly advanced example of overriding default behavior and theme
