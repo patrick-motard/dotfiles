bddc385fe9e58b71a4d32a1b3f47bc41cde5b31f
Patrick Motard <motard19@gmail.com>
2024-12-30 15:46:36 -0600

    nvim: lazy.nvim: disabled change detection notifications.

--- dot_config/nvim-custom/init.lua
+++ dot_config/nvim-custom/init.lua
@@ -75,8 +75,18 @@ vim.opt.rtp:prepend(lazypath)
 --  To update plugins you can run
 --    :Lazy update
 --
+--
+-- Lazy.nvim options:
+local options = {
+
+  change_detection = {
+    enabled = true,
+    notify = false,
+  },
+}
+
 -- NOTE: Here is where you install your plugins.
-require('lazy').setup({
+local plugins = {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
@@ -639,30 +649,12 @@ require('lazy').setup({
   },
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { import = 'plugins' },
-}, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
-})
+}
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+require('lazy').setup(plugins, options)
 
 require 'config.keymaps'
 require 'config.autocmds'
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
