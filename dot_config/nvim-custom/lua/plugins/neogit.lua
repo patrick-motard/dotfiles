975a1300de62bc3ed3f0bb1b66d36f7842edcb85
Patrick Motard <motard19@gmail.com>
2025-01-01 00:24:55 -0600

    nvim: added neogit
    just trying it out. i used to use magit in emacs and liked it quite a
    bit. this is based on magit. however, so far I think LazyGit is better.

---
+++ dot_config/nvim-custom/lua/plugins/neogit.lua
@@ -0,0 +1,11 @@
+return {
+  'NeogitOrg/neogit',
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- required
+    'sindrets/diffview.nvim', -- optional - Diff integration
+
+    -- Only one of these is needed.
+    'nvim-telescope/telescope.nvim', -- optional
+  },
+  config = true,
+}
