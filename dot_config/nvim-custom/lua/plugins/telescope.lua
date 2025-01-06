d932070358190869d700c64f4aa4d12574709c98
Patrick Motard <motard19@gmail.com>
2025-01-06 16:54:15 -0600

    neovim: added a few telescope searches
    - grep in plugins
    - multigrep. first word is search term. second word is a glob pattern
    for files to search over.

--- dot_config/nvim-custom/lua/plugins/telescope.lua
+++ dot_config/nvim-custom/lua/plugins/telescope.lua
@@ -137,5 +137,13 @@ return {
     vim.keymap.set('n', '<leader>sn', function()
       builtin.find_files { cwd = vim.fn.stdpath 'config' }
     end, { desc = '[S]earch [N]eovim files' })
+
+    vim.keymap.set('n', '<leader>sp', function()
+      require('telescope.builtin').find_files {
+        cwd = vim.fs.joinpath(vim.fn.stdpath 'data', 'lazy'),
+      }
+    end, { desc = '[S]earch [P]lugin files' })
+
+    require('config.telescope.multigrep').setup()
   end,
 }
