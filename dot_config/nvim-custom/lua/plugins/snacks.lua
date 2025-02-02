9a1bf9d2f3daa281ab73b3f82d9f8e22923e284f
Patrick Motard <motard19@gmail.com>
2025-02-01 21:40:53 -0600

    added snacks nvim

---
+++ dot_config/nvim-custom/lua/plugins/snacks.lua
@@ -0,0 +1,60 @@
+require 'plugins.custom.git'
+
+return {
+  'folke/snacks.nvim',
+  ---@type snacks.Config
+  opts = {
+    ---@type snacks.picker.Config
+    picker = {
+      sources = {
+        git_log_file = {
+          confirm = function(picker, item)
+            openPullRequest(item.commit)
+            picker:close()
+          end,
+          actions = {
+            foo = function(picker, item)
+              vim.notify 'foo'
+            end,
+            bar = function(picker, item)
+              vim.notify 'bar'
+            end,
+          },
+          win = {
+            input = {
+
+              keys = {
+                ['<cr>'] = 'confirm',
+                ['<a-c>'] = { 'foo', mode = { 'n', 'i' } },
+                ['<a-d>'] = { 'bar', mode = { 'n', 'i' } },
+              },
+            },
+          },
+        },
+      },
+      layout = 'default',
+      -- ---@type snacks.picker.layout.Config
+      -- layout = {
+      --
+      --   layout = {
+      --     box = 'horizontal',
+      --     title = '{title} {flags}',
+      --     title_position = 'center',
+      --     { win = 'input', height = 20, border = 'rounded' },
+      --   },
+      -- },
+      --
+      -- title = 'Snacks',
+    },
+  },
+  keys = {
+    {
+      '<leader>as',
+      function()
+        local Snacks = require 'snacks'
+        Snacks.picker.git_log_file()
+      end,
+      desc = 'Snacks',
+    },
+  },
+}
