f091ab72977243eb412c88a7f50ab10795b932f2
Patrick Motard <motard19@gmail.com>
2026-02-01 20:44:35 -0600

    Update treesitter configs for nvim-treesitter 1.0+ compatibility
    nvim-treesitter 1.0 removed the nvim-treesitter.configs module entirely,
    breaking the old configuration approach. This update:
    
    - treesitter.lua: Remove deprecated configs module usage, simplify to
      just register mdx as markdown and set up incremental selection keymaps
    - treesitter-textobjects.lua: Switch to main branch (old master is frozen),
      use new nvim-treesitter-textobjects module API with explicit keymap setup
      for select, move, swap, and repeatable move functionality

--- dot_config/nvim-custom/lua/plugins/treesitter.lua
+++ dot_config/nvim-custom/lua/plugins/treesitter.lua
@@ -1,60 +1,21 @@
--- https://www.josean.com/posts/nvim-treesitter-and-textobjects
+-- https://github.com/nvim-treesitter/nvim-treesitter
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   event = { 'BufReadPre', 'BufNewFile' },
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
+  cmd = { 'TSInstall', 'TSUpdate', 'TSUpdateSync' },
   config = function()
-    local treesitter = require 'nvim-treesitter.configs'
+    vim.treesitter.language.register('markdown', 'mdx')
 
-    treesitter.setup {
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter.
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-      auto_install = true,
-      indent = { enable = true, disable = { 'ruby' } },
-      ensure_installed = {
-        'bash',
-        'html',
-        'javascript',
-        'json',
-        'diff',
-        'lua',
-        'luadoc',
-        'markdown',
-        'markdown_inline',
-        'python',
-        'query',
-        'regex',
-        'ruby',
-        'tsx',
-        'typescript',
-        'vim',
-        'vimdoc',
-        'yaml',
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<C-space>',
-          node_incremental = '<C-space>',
-          scope_incremental = false,
-          node_decremental = '<bs>',
-        },
-      },
-    }
+    -- Incremental selection keymaps
+    vim.keymap.set('n', '<C-space>', function()
+      require('nvim-treesitter.incremental_selection').init_selection()
+    end, { desc = 'Start incremental selection' })
+    vim.keymap.set('v', '<C-space>', function()
+      require('nvim-treesitter.incremental_selection').node_incremental()
+    end, { desc = 'Increment selection' })
+    vim.keymap.set('v', '<bs>', function()
+      require('nvim-treesitter.incremental_selection').node_decremental()
+    end, { desc = 'Decrement selection' })
   end,
 }
