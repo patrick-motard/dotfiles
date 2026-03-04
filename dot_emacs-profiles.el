39a0ebc5d5b5baf5c1fa175f79dac74623e3f3a4
Patrick Motard <motard19@gmail.com>
2026-03-04 15:19:34 -0600

    Set han as default Emacs profile and add MCP server to ansible
    - Change default Emacs profile from doom to han in emacs-profiles.el
    - Add ansible task to clone rhblind/emacs-mcp-server for Claude Code
      integration (referenced in han's init.el)

--- dot_emacs-profiles.el
+++ dot_emacs-profiles.el
@@ -1,3 +1,3 @@
-(("default" . ((user-emacs-directory . "~/.emacs.d.doom")
-               (env . (("DOOMDIR" . "~/.doom.d")))))
- ("han"     . ((user-emacs-directory . "~/.emacs.d.han"))))
+(("default" . ((user-emacs-directory . "~/.emacs.d.han")))
+ ("doom"    . ((user-emacs-directory . "~/.emacs.d.doom")
+               (env . (("DOOMDIR" . "~/.doom.d"))))))
