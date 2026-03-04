289282612e4f910998e945d8e1b8bcd8eab444c2
Patrick Motard <motard19@gmail.com>
2026-03-04 15:19:40 -0600

    Update neovim config: consolidate treesitter, add keymaps and LSP shim
    - Merge treesitter-textobjects config into treesitter.lua (remove separate file)
    - Add scratch buffer keymap (bn) for quick temporary buffers
    - Add diffview.nvim keybindings under <leader>g for file/repo history
    - Add compatibility shim for deprecated vim.lsp.util.jump_to_location API
      to bridge to new show_document API until plugins migrate

--- dot_config/nvim-custom/lua/plugins/diffview.lua
+++ dot_config/nvim-custom/lua/plugins/diffview.lua
@@ -1,3 +1,10 @@
 return {
   'sindrets/diffview.nvim',
+  cmd = { 'DiffviewOpen', 'DiffviewFileHistory', 'DiffviewClose' },
+  keys = {
+    { '<leader>gh', '<cmd>DiffviewFileHistory %<cr>', desc = 'File History' },
+    { '<leader>gH', '<cmd>DiffviewFileHistory<cr>', desc = 'Repo History' },
+    { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = 'Diff View (Changes)' },
+    { '<leader>gq', '<cmd>DiffviewClose<cr>', desc = 'Close Diff View' },
+  },
 }
