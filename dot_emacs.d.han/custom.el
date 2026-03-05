0cd006c711ef4bd25037ecfd2a1afe69f4b836d4
Patrick Motard <motard19@gmail.com>
2026-03-04 21:18:54 -0600

    Track emacs-profile and custom.el, ignore Emacs autosave files
    - dot_emacs-profile: chemacs2 profile selector (points to 'han' config)
    - dot_emacs.d.han/custom.el: Emacs custom-set-variables separated from init.el
    - .gitignore: add pattern to ignore Emacs autosave/lock files (#*#)

---
+++ dot_emacs.d.han/custom.el
@@ -0,0 +1,53 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2E3440" "#C16069" "#A2BF8A" "#ECCC87" "#80A0C2" "#B58DAE" "#86C0D1"
+    "#ECEFF4"])
+ '(custom-safe-themes
+   '("3613617b9953c22fe46ef2b593a2e5bc79ef3cc88770602e7e569bbd71de113b"
+     "7f791f743870983b9bb90c8285e1e0ba1bf1ea6e9c9a02c60335899ba20f3c94"
+     "2277b74ae6f5aa018aa0057ef89752163e34fcb09ab6242f169c1740a72ca27a"
+     "34c99997eaa73d64b1aaa95caca9f0d64229871c200c5254526d0062f8074693"
+     "e3c87e869f94af65d358aa279945a3daf46f8185f1a5756ca1c90759024593dd"
+     default))
+ '(fci-rule-color "#4C566A")
+ '(helm-minibuffer-history-key "M-p")
+ '(jdee-db-active-breakpoint-face-colors (cons "#191C25" "#80A0C2"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#191C25" "#A2BF8A"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#191C25" "#434C5E"))
+ '(objed-cursor-color "#C16069")
+ '(org-link-frame-setup
+   '((vm . vm-visit-folder-other-frame)
+     (vm-imap . vm-visit-imap-folder-other-frame)
+     (gnus . org-gnus-no-new-news) (file . find-file)
+     (wl . wl-other-frame)))
+ '(package-selected-packages nil)
+ '(package-vc-selected-packages
+   '((claude-code-ide :vc-backend Git :url
+		      "https://github.com/manzaltu/claude-code-ide.el")))
+ '(pdf-view-midnight-colors (cons "#ECEFF4" "#2E3440"))
+ '(rustic-ansi-faces
+   ["#2E3440" "#BF616A" "#A3BE8C" "#EBCB8B" "#81A1C1" "#B48EAD" "#88C0D0"
+    "#ECEFF4"])
+ '(send-mail-function 'mailclient-send-it)
+ '(vc-annotate-background "#2E3440")
+ '(vc-annotate-color-map
+   (list (cons 20 "#A2BF8A") (cons 40 "#bac389") (cons 60 "#d3c788")
+	 (cons 80 "#ECCC87") (cons 100 "#e3b57e") (cons 120 "#da9e75")
+	 (cons 140 "#D2876D") (cons 160 "#c88982")
+	 (cons 180 "#be8b98") (cons 200 "#B58DAE")
+	 (cons 220 "#b97e97") (cons 240 "#bd6f80")
+	 (cons 260 "#C16069") (cons 280 "#a15b66")
+	 (cons 300 "#825663") (cons 320 "#625160")
+	 (cons 340 "#4C566A") (cons 360 "#4C566A")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
