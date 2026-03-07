634573b37e8d090a75d628d2fb44aad1aaa9d5d5
Patrick Motard <motard19@gmail.com>
2026-03-07 15:42:00 -0600

    Fix neovim-project configuration
    Remove unnecessary module require that was causing errors. Use direct
    Telescope commands instead of programmatic API calls.

--- dot_config/nvim-custom/lua/plugins/project.lua
+++ dot_config/nvim-custom/lua/plugins/project.lua
@@ -25,30 +25,15 @@ return {
       dashboard_mode = false,
     }
 
-    -- Load the telescope extension
-    local neovim_project_discover = require 'neovim-project.discover'
-    require('telescope').load_extension 'neovim-project'
-
     -- Keybindings for project management
-    local telescope = require 'telescope'
     local builtin = require 'telescope.builtin'
 
     -- Normal mode keybindings with <leader>p prefix (like Projectile)
-    vim.keymap.set('n', '<leader>pp', function()
-      telescope.extensions['neovim-project'].discover {}
-    end, { desc = '[P]roject switch [P]roject' })
-    vim.keymap.set('n', '<leader>ph', function()
-      telescope.extensions['neovim-project'].history {}
-    end, { desc = '[P]roject [H]istory' })
+    vim.keymap.set('n', '<leader>pp', '<cmd>Telescope neovim-project discover<cr>', { desc = '[P]roject switch [P]roject' })
+    vim.keymap.set('n', '<leader>ph', '<cmd>Telescope neovim-project history<cr>', { desc = '[P]roject [H]istory' })
     vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = '[P]roject [F]ind files' })
     vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = '[P]roject [S]earch (grep)' })
     vim.keymap.set('n', '<leader>pr', builtin.oldfiles, { desc = '[P]roject [R]ecent files' })
-    vim.keymap.set('n', '<leader>pw', function()
-      telescope.extensions['neovim-project'].discover {}
-    end, { desc = '[P]roject [W]orkspace (change directory)' })
-
-    -- The neovim-project picker already has good defaults
-    -- Press Enter to switch to project
-    -- It will open the last session or create a new one
+    vim.keymap.set('n', '<leader>pw', '<cmd>Telescope neovim-project discover<cr>', { desc = '[P]roject [W]orkspace (change directory)' })
   end,
 }
