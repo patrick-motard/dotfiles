6c6d2a15d31645996e7a5c0503e6d959940ea761
Patrick Motard <motard19@gmail.com>
2026-03-09 21:52:30 -0500

    Misc config improvements - noice background, octo my PRs, lazygit nvim-remote, gh-dash lazygit keybind

---
+++ dot_config/nvim-custom/lua/plugins/noice.lua
@@ -0,0 +1,30 @@
+return {
+  'folke/noice.nvim',
+  event = 'VeryLazy',
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    {
+      'rcarriga/nvim-notify',
+      opts = {
+        background_colour = '#000000',
+      },
+    },
+  },
+  opts = {
+    lsp = {
+      -- override markdown rendering so that cmp and other plugins use Treesitter
+      override = {
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true,
+      },
+    },
+    presets = {
+      bottom_search = true,        -- use a classic bottom cmdline for search
+      command_palette = true,      -- position the cmdline and popupmenu together at the top
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = false,
+      lsp_doc_border = true,       -- add a border to hover docs and signature help
+    },
+  },
+}
