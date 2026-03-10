fa698e4e8f43725b507b2dbf8b6e1316a56fb269
Patrick Motard <motard19@gmail.com>
2026-03-09 21:52:17 -0500

    Set localleader to comma

--- dot_config/nvim-custom/lua/config/options.lua
+++ dot_config/nvim-custom/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = ','
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
