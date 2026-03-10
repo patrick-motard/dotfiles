a268af861c4bfb599b27a92eba25d2131492e9ad
Patrick Motard <motard19@gmail.com>
2026-03-09 22:56:14 -0500

    Restore MenuHammer as the interface for Hammerspoon actions
    Move reload and cheatsheet actions into MenuHammer menu instead of
    direct hotkeys. MenuHammer (hyper+m) is the single entry point.

--- dot_hammerspoon/menuHammerCustomConfig.lua
+++ dot_hammerspoon/menuHammerCustomConfig.lua
@@ -1,53 +1,19 @@
--- This is used by menuHammer
--- Menuhammer is loaded in init.lua
--- https://github.com/FryJay/MenuHammer
--- https://github.com/cldwalker/hammerspoon-files/blob/0bdeb21a4764bf8fafe9fb2b67de9fff7e902f94/menuHammerCustomConfig.lua
+-- MenuHammer config - https://github.com/FryJay/MenuHammer
 menuHammerMenuList = {
 	mainMenu = {
 		parentMenu = nil,
-		-- menuHotkey = {{'alt'}, 'space'},
 		menuHotkey = { hyper, "m" },
 		menuItems = {
-			{ cons.cat.submenu, "", "O", "Open", { { cons.act.menu, "openMenu" } } },
-			{ cons.cat.action, "", "1", "VSCode", { { cons.act.launcher, "Visual Studio Code" } } },
-			{ cons.cat.action, "", "2", "Alacritty", { { cons.act.launcher, "Alacritty" } } },
-			{ cons.cat.action, "", "3", "Firefox", { { cons.act.launcher, "Firefox" } } },
-			{ cons.cat.action, "", "4", "Slack", { { cons.act.launcher, "Slack" } } },
-			{ cons.cat.action, "", "0", "Spotify", { { cons.act.launcher, "Spotify" } } },
-		},
-	},
-	openMenu = {
-		parentMenu = "mainMenu",
-		menuHotkey = nil,
-		menuItems = {
-			{ cons.cat.action, "", "T", "Iterm", { { cons.act.launcher, "Iterm" } } },
-			{
-				cons.cat.action,
-				"",
-				"H",
-				"Hints",
-				{ {
-					cons.act.func,
-					function()
-						openHints()
-					end,
-				} },
-			},
+			{ cons.cat.action, "", "r", "Reload Hammerspoon", { { cons.act.func, function()
+				hs.notify.new({ informativeText = "Reloading Hammerspoon." }):send()
+				hs.reload()
+			end } } },
+			{ cons.cat.action, "", "k", "Show Keybindings", { { cons.act.func, function()
+				spoon.HSKeybindings:show()
+			end } } },
+			{ cons.cat.action, "", "/", "KSheet Toggle", { { cons.act.func, function()
+				spoon.KSheet:toggle()
+			end } } },
 		},
 	},
 }
-
-openHints = function()
-	hs.hints.showTitleThresh = 10
-	-- hs.hints.style = 'vimperator'
-	hs.hints.windowHints()
-end
-
--- -- Use to get name of application
--- -- hs.hotkey.bind(hyper, '8', function()
--- --   print(hs.application.find('code'))
--- -- end)
--- hs.hotkey.bind(hyper, 't', 'Tabs', function()
---   app = hs.application.frontmostApplication()
---   hs.tabs.enableForApp(app)
--- end)
