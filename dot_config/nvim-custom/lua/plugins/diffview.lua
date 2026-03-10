1f8cab38428107b9ef51aee31119fc4dce5bce28
Patrick Motard <motard19@gmail.com>
2026-03-09 21:52:26 -0500

    Remap diffview internal keys to localleader to avoid leader conflicts

--- dot_config/nvim-custom/lua/plugins/diffview.lua
+++ dot_config/nvim-custom/lua/plugins/diffview.lua
@@ -7,4 +7,32 @@ return {
     { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = '[d]iff view' },
     { '<leader>gq', '<cmd>DiffviewClose<cr>', desc = '[q]uit diff view' },
   },
+  opts = {
+    keymaps = {
+      view = {
+        { 'n', '<localleader>e',  '<cmd>DiffviewFocusFiles<cr>',                { desc = 'Bring focus to the file panel' } },
+        { 'n', '<localleader>b',  '<cmd>DiffviewToggleFiles<cr>',               { desc = 'Toggle the file panel' } },
+        { 'n', '<localleader>co', function() require('diffview.actions').conflict_choose('ours') end,   { desc = 'Choose OURS' } },
+        { 'n', '<localleader>ct', function() require('diffview.actions').conflict_choose('theirs') end, { desc = 'Choose THEIRS' } },
+        { 'n', '<localleader>cb', function() require('diffview.actions').conflict_choose('base') end,   { desc = 'Choose BASE' } },
+        { 'n', '<localleader>ca', function() require('diffview.actions').conflict_choose('all') end,    { desc = 'Choose all' } },
+        { 'n', '<localleader>cO', function() require('diffview.actions').conflict_choose_all('ours') end,   { desc = 'Choose OURS (whole file)' } },
+        { 'n', '<localleader>cT', function() require('diffview.actions').conflict_choose_all('theirs') end, { desc = 'Choose THEIRS (whole file)' } },
+        { 'n', '<localleader>cB', function() require('diffview.actions').conflict_choose_all('base') end,   { desc = 'Choose BASE (whole file)' } },
+        { 'n', '<localleader>cA', function() require('diffview.actions').conflict_choose_all('all') end,    { desc = 'Choose all (whole file)' } },
+      },
+      file_panel = {
+        { 'n', '<localleader>e', '<cmd>DiffviewFocusFiles<cr>',  { desc = 'Bring focus to the file panel' } },
+        { 'n', '<localleader>b', '<cmd>DiffviewToggleFiles<cr>', { desc = 'Toggle the file panel' } },
+        { 'n', '<localleader>cO', function() require('diffview.actions').conflict_choose_all('ours') end,   { desc = 'Choose OURS (whole file)' } },
+        { 'n', '<localleader>cT', function() require('diffview.actions').conflict_choose_all('theirs') end, { desc = 'Choose THEIRS (whole file)' } },
+        { 'n', '<localleader>cB', function() require('diffview.actions').conflict_choose_all('base') end,   { desc = 'Choose BASE (whole file)' } },
+        { 'n', '<localleader>cA', function() require('diffview.actions').conflict_choose_all('all') end,    { desc = 'Choose all (whole file)' } },
+      },
+      file_history_panel = {
+        { 'n', '<localleader>e', '<cmd>DiffviewFocusFiles<cr>',  { desc = 'Bring focus to the file panel' } },
+        { 'n', '<localleader>b', '<cmd>DiffviewToggleFiles<cr>', { desc = 'Toggle the file panel' } },
+      },
+    },
+  },
 }
