b0b30e8f0b6c1e437abf24edd5a8b92a4e780c59
Patrick Motard <motard19@gmail.com>
2026-06-04 21:42:28 -0500

    Route startup directory buffers to intro screen
    Keep Oil as the default file explorer for normal directory opens, but replace startup and session-restored directory buffers with Neovim's intro screen so opening a project root does not drop into netrw.

--- dot_config/nvim-custom/lua/config/autocmds.lua
+++ dot_config/nvim-custom/lua/config/autocmds.lua
@@ -29,3 +29,36 @@ vim.api.nvim_create_autocmd('SwapExists', {
     vim.v.swapchoice = 'e'
   end,
 })
+
+local function replace_startup_directory_with_intro()
+  if vim.fn.argc() ~= 1 then
+    return
+  end
+
+  local arg = vim.fn.argv(0)
+  if vim.fn.isdirectory(arg) == 0 then
+    return
+  end
+
+  local current_buf_name = vim.api.nvim_buf_get_name(0)
+  if current_buf_name == '' or vim.fn.isdirectory(current_buf_name) == 0 then
+    return
+  end
+
+  vim.cmd.bwipeout()
+  vim.cmd.enew()
+  vim.cmd.intro()
+end
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    vim.schedule(replace_startup_directory_with_intro)
+  end,
+})
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'SessionLoadPost',
+  callback = function()
+    vim.schedule(replace_startup_directory_with_intro)
+  end,
+})
