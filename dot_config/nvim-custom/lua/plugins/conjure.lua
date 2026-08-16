2e7304e64a65b97a12598a8d283728fe70fd0637
Patrick Motard <motard19@gmail.com>
2026-08-16 00:55:41 -0500

    Fix Clojure localleader keymap conflict

--- dot_config/nvim-custom/lua/plugins/conjure.lua
+++ dot_config/nvim-custom/lua/plugins/conjure.lua
@@ -5,5 +5,23 @@ return {
     vim.g['conjure#mapping#prefix'] = '<localleader>'
     require('conjure.main').main()
     require('conjure.mapping')['on-filetype']()
+
+    -- nvim-treesitter-textobjects maps bare `,` globally for repeat-move,
+    -- which shadows which-key's localleader trigger. Override it in Conjure
+    -- buffers so pressing the localleader opens the action menu.
+    local function setup_localleader_keymap(buf)
+      vim.keymap.set('n', vim.g.maplocalleader, function()
+        require('which-key').show { keys = vim.g.maplocalleader, loop = true }
+      end, { buffer = buf, desc = 'which-key conjure actions' })
+    end
+
+    setup_localleader_keymap(0)
+    vim.api.nvim_create_autocmd('FileType', {
+      group = vim.api.nvim_create_augroup('conjure-which-key-localleader', { clear = true }),
+      pattern = { 'clojure', 'edn' },
+      callback = function(event)
+        setup_localleader_keymap(event.buf)
+      end,
+    })
   end,
 }
