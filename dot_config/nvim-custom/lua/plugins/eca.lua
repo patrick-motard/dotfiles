b63395378193288985f0eba761f666d21e4374f6
Patrick Motard <motard19@gmail.com>
2026-09-04 00:45:02 -0500

    Add ECA Neovim integration

---
+++ dot_config/nvim-custom/lua/plugins/eca.lua
@@ -0,0 +1,11 @@
+-- ECA (Editor Code Assistant) chat sidebar for Neovim
+-- https://github.com/editor-code-assistant/eca-nvim
+return {
+  'editor-code-assistant/eca-nvim',
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'nvim-lua/plenary.nvim',
+    'folke/snacks.nvim',
+  },
+  opts = {},
+}
